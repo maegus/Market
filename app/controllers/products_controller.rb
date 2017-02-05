aeabc268c0754ace148a55e135886280162a9b74
Draveness <stark.draven@gmail.com>
2017-02-05 21:25:26 +0800

    Fix typo in product controller and jbuilder

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -43,6 +43,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:image, :title, :price, :descritpion)
+      params.require(:product).permit(:image, :title, :price, :description)
     end
 end
--- app/views/products/_product.json.jbuilder
+++ app/views/products/_product.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! product, :id, :image, :title, :price, :descritpion, :created_at, :updated_at
+json.extract! product, :id, :image, :title, :price, :description, :created_at, :updated_at
 json.url product_url(product, format: :json)
